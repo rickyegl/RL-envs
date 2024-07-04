f23a9861b6e80c4785269253a4c0f12bd2722487
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-07-03 21:20:43 -0600

    Update qbert_rainbow_config.py

--- qbert_rainbow_config.py
+++ qbert_rainbow_config.py
@@ -41,7 +41,7 @@ qbert_rainbow_config = dict(
                 end=0.05,
                 decay=1000000,
             ),
-            replay_buffer=dict(replay_buffer_size=1000, ),
+            replay_buffer=dict(replay_buffer_size=4000, ),
         ),
     ),
 )
